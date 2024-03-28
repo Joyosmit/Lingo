546e9b3f1b079ca60201ca57c0631fccc62a57ed
Joyosmit <joyosmitp@gmail.com>
2024-03-28 20:01:10 +0530

    Started working on the Database by using Neon for serverless PostgreSQL, and Drizzle ORM

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,21 +1,24 @@
-import { MobileHeader } from "@/components/mobile-header"
-import { Sidebar } from "@/components/sidebar"
+import { Sidebar } from "@/components/sidebar";
+import { MobileHeader } from "@/components/mobile-header";
 
 type Props = {
-    children: React.ReactNode
-}
+    children: React.ReactNode;
+};
 
-const MainLayout = ({ children }: Props) => {
+const MainLayout = ({
+    children,
+}: Props) => {
     return (
         <>
-            <MobileHeader/>
-            <Sidebar className="hidden lg:flex"/>
+            <MobileHeader />
+            <Sidebar className="hidden lg:flex" />
             <main className="lg:pl-[256px] h-full pt-[50px] lg:pt-0">
-                <div className="bg-red-500 h-full">
+                <div className="max-w-[1056px] w-full mx-auto pt-6 h-full">
                     {children}
                 </div>
             </main>
         </>
-    )
-}
-export default MainLayout
+    );
+};
+
+export default MainLayout;
